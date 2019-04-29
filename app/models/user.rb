863a29d5bb434d9a7c41f15028fbee8232d161d2
sjscotton <sj.scotton@gmail.com>
2019-04-29 15:08:07 -0700

    added login via OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
-  validates :username, uniqueness: true, presence: true
+  validates :uid, uniqueness: true, presence: true
+
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = "github"
+    user.name = auth_hash["info"]["name"]
+    user.email = auth_hash["info"]["email"]
+
+    # Note that the user has not been saved.
+    # We'll choose to do the saving outside of this method
+    return user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407164321) do
+ActiveRecord::Schema.define(version: 2019_04_29_213439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "uid"
+    t.string "provider"
+    t.string "name"
+    t.string "email"
   end
 
-  create_table "votes", force: :cascade do |t|
+  create_table "votes", id: :serial, force: :cascade do |t|
     t.integer "user_id"
     t.integer "work_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
-    t.index ["work_id"], name: "index_votes_on_work_id", using: :btree
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
-  create_table "works", force: :cascade do |t|
+  create_table "works", id: :serial, force: :cascade do |t|
     t.string "title"
     t.string "creator"
     t.string "description"
