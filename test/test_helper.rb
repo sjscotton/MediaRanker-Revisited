f22bfe9b7148b0884d45d2f6af090bfcf2977f4d
sjscotton <sj.scotton@gmail.com>
2019-05-13 09:48:04 -0700

    Added perform login helper method

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -40,4 +40,13 @@ class ActiveSupport::TestCase
              },
            }
   end
+
+  def perform_login(user = nil)
+    user ||= User.first
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get auth_callback_path(:github)
+
+    return user
+  end
 end
