e731982213ebcb7f710aad21aacfed922b681958
sjscotton <sj.scotton@gmail.com>
2019-05-13 08:20:01 -0700

    all test pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
-  validates :user, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true
 
   def self.build_from_github(auth_hash)
     user = User.new
